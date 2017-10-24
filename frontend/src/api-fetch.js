aec07c74e076126c36740ed9df6ad6c8690554d9
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-10-23 22:46:45 -0400

    feat: add api-fetch.js for making server requests

---
+++ frontend/src/api-fetch.js
@@ -0,0 +1,116 @@
+const uuidv4 = require('uuid/v4')
+
+const api = 'http://localhost:3001'
+
+localStorage.token = localStorage.token || Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': localStorage.token
+}
+
+const fetchJson = (url, opts) => fetch(url, opts).then(res => res.json())
+
+export const getCategories = () => {
+  return fetchJson(`${api}/categories`, { headers })
+}
+
+export const getCategoryPosts = (category) => {
+  return fetchJson(`${api}/${category}/posts`, { headers })
+}
+
+export const getPosts = () => {
+  return fetchJson(`${api}/posts`, { headers })
+}
+
+export const addPost = (title, body, author, category) => {
+  return fetch(`${api}/posts`, {
+    method: 'POST',
+    headers,
+    body: {
+      id: uuidv4(),
+      timestamp: Date.now(),
+      title,
+      body,
+      author,
+      category
+    }
+  })
+}
+
+export const getPost = (id) => {
+  return fetchJson(`${api}/posts/${id}`, { headers })
+}
+
+export const voteOnPost = (id, option) => {
+  return fetch(`${api}/posts/${id}`, {
+    method: 'POST',
+    headers,
+    body: {
+      option
+    }
+  })
+}
+
+export const editPost = (id, title, body) => {
+  return fetch(`${api}/posts/${id}`, {
+    method: 'PUT',
+    headers,
+    body: {
+      title,
+      body
+    }
+  })
+}
+
+export const deletePost = (id) => {
+  return fetch(`${api}/posts/${id}`, { method: 'DELETE', headers })
+}
+
+export const getPostComments = (id) => {
+  return fetchJson(`${api}/posts/${id}/comments`, { headers })
+}
+
+export const addComment = (body, author, parentId) => {
+  return fetch(`${api}/comments`, {
+    method: 'POST',
+    headers,
+    body: {
+      id: uuidv4(),
+      timestamp: Date.now(),
+      body,
+      author,
+      parentId
+    }
+  })
+}
+
+export const getComment = (id) => {
+  return fetchJson(`${api}/comments/${id}`, { headers })
+}
+
+export const voteOnComment = (id, option) => {
+  return fetch(`${api}/comments/${id}`, {
+    method: 'POST',
+    headers,
+    body: { option }
+  })
+}
+
+export const editComment = (id, body) => {
+  return fetch(`${api}/comments/${id}`, {
+    method: 'PUT',
+    headers,
+    body: {
+      timestamp: Date.now(),
+      body
+    }
+  })
+}
+
+export const deleteComment = (id) => {
+  return fetch(`${api}/comments/${id}`, {
+    method: 'DELETE',
+    headers
+  })
+}
