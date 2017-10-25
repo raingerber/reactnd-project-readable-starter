e17bddd6be9c16631890392f1b6617492f583fbe
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-10-25 15:41:21 -0400

    feat: use combineReducers with post, comment, and category reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,19 @@
-function reducer (state = {}, action) {
+import { combineReducers } from 'react-redux'
+
+function postReducer (state = {}, action) {
   return state
 }
 
-export default reducer
+function commentReducer (state = {}, action) {
+  return state
+}
+
+function categoryReducer (state = {}, action) {
+  return state
+}
+
+export default combineReducers({
+  posts: postReducer,
+  comments: commentReducer,
+  categories: categoryReducer
+})
