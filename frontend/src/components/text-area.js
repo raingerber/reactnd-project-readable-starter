2c4e1383ea1e074508aebe4b5ada8c5d8320ec99
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-10-28 09:55:49 -0400

    feat: update textarea component

--- frontend/src/components/text-area.js
+++ frontend/src/components/text-area.js
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types'
 class TextArea extends Component {
   constructor (props) {
     super(props)
-    this.state = { value: '' }
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.state = { value: props.value }
   }
 
   onChange (event) {
@@ -15,29 +13,26 @@ class TextArea extends Component {
 
   onSubmit (event) {
     event.preventDefault()
-    this.props.onSubmit(this.state.value)
   }
 
   render () {
     return (
-      <div className='form-item'>
-        <form onSubmit={this.onSubmit}>
-          <label>
-            <textarea value={this.state.value} onChange={this.onChange} rows={this.props.rows}/>
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+      <textarea
+        value={this.state.value}
+        onChange={this.onChange.bind(this)}
+        rows={this.props.rows}
+      />
     )
   }
 }
 
 TextArea.propTypes = {
   rows: PropTypes.number,
-  onSubmit: PropTypes.func.isRequired
+  value: PropTypes.string
 }
 
 TextArea.defaultProps = {
+  value: '',
   rows: 6
 }
 
