3abffc35333e45295c10df427993d7af5e123fa6
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-10-29 09:31:13 -0400

    feat: add react-router to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import thunk from 'redux-thunk'
+import reducer from './reducers/index'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
