7d8928a7ee17efadf77764e62de96ea7dd7f9a89
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-10-30 10:01:47 -0400

    feat: add message when PostList component is empty

--- frontend/src/components/post-list.js
+++ frontend/src/components/post-list.js
@@ -5,11 +5,20 @@ import { connect } from 'react-redux'
 import Post from './post'
 
 function PostList ({ posts }) {
+  // console.error('rendering PostList', posts)
+  if (posts.length) {
+    return (
+      <div className='post-list'>
+        {posts.map((post) => <Post key={post.id} {...post} />)}
+      </div>
+    )
+  }
+
   return (
-    <div className='post-list'>
-      {posts.map(post => <Post key={post.id} {...post} />)}
+    <div className='text-block text-center'>
+      <i>No posts here yet!</i>
     </div>
   )
 }
 
-export default connect(({ posts }) => ({ posts }))(PostList)
+export default connect(({ posts: { posts } }) => ({ posts }))(PostList)
