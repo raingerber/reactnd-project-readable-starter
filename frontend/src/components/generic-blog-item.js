432e86ed311111cca069f242996e344c0ce0428c
Matthew Armstrong <matthewr.armstrong@viacom.com>
2017-10-30 10:03:24 -0400

    feat: simplify jsx for GenericBlogItem component

--- frontend/src/components/generic-blog-item.js
+++ frontend/src/components/generic-blog-item.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import { formatUnixTimestamp } from '../utils'
 
+import { Button, CaretButton } from './button'
 import EditableText from './editable-text'
 
 class GenericBlogItem extends Component {
@@ -10,8 +12,8 @@ class GenericBlogItem extends Component {
     super(props)
     this.state = {
       editMode: false, // TODO move this to the store
-      title: props.title,
-      body: props.body
+      title: props.title, // used for controlled component
+      body: props.body // used for controlled component
     }
   }
 
@@ -48,63 +50,55 @@ class GenericBlogItem extends Component {
 
   render () {
     return (
-      <div className={this.state.editMode ? 'editing' : ''}>
-        <form className='form' onSubmit={this.onSubmit.bind(this)}>
-          <fieldset>
-            <legend>{`${this.props.type} from ${formatUnixTimestamp(Date.now())}`}</legend>
-            {this.props.useTitle && <div className='form-item'>
-              <label>Title:</label>
-              <EditableText
-                text={this.state.title}
-                editMode={this.state.editMode}
-                onChange={this.updateTitle.bind(this)}
-              />
-            </div>}
-            <div>
-              <div className='form-item'>
-                <label>Body:</label>
-                <EditableText
-                  text={this.state.body}
-                  editMode={this.state.editMode}
-                  onChange={this.updateBody.bind(this)}
-                />
-              </div>
-              <div className='button-group'>
-                {this.state.editMode && (
-                  <button className='button' onClick={this.saveChanges.bind(this)}>Save</button>
-                )}
-                {this.state.editMode && (
-                  <button className='button' onClick={this.removeChanges.bind(this)}>Remove Changes</button>
-                )}
-                {!this.state.editMode && (
-                  <button className='button' onClick={this.setEditMode.bind(this, true)}>Edit</button>
-                )}
-                <button className='button'>Delete</button>
-                <RoundButton onClick={this.props.upVoteItem}>
-                  <span className='button-text'>Up</span>
-                  <span className='caret up' />
-                </RoundButton>
-                <RoundButton onClick={this.props.downVoteItem}>
-                  <span className='button-text'>Down</span>
-                  <span className='caret down' />
-                </RoundButton>
-              </div>
-              <div>
-                <span>Votes:</span>
-                <span className='label badge black'>
-                  {this.props.voteScore}
-                </span>
-              </div>
-              <div>
-                <span>Comments:</span>
-                <span className='label badge black'>
-                  {this.props.commentCount}
-                </span>
-              </div>
-            </div>
-          </fieldset>
-        </form>
-      </div>
+      <form className='form post-form' onSubmit={this.onSubmit.bind(this)}>
+        <fieldset>
+          <legend>{`${this.props.type} from ${formatUnixTimestamp(Date.now())}`}</legend>
+          <div className='vote-panel'>
+            <CaretButton onClick={this.props.upVoteItem} outline>
+              <span className='caret up' />
+            </CaretButton>
+            <CaretButton onClick={this.props.downVoteItem} outline>
+              <span className='caret down' />
+            </CaretButton>
+            <VoteScoreBadge voteScore={this.props.voteScore} />
+          </div>
+          {this.props.useTitle && <div className='form-item'>
+            <label>Title:</label>
+            <EditableText
+              rows='1'
+              text={this.state.title}
+              editMode={this.state.editMode}
+              onChange={this.updateTitle.bind(this)}
+            />
+          </div>}
+          <div className='form-item'>
+            <label>Body:</label>
+            <EditableText
+              rows='3'
+              text={this.state.body}
+              editMode={this.state.editMode}
+              onChange={this.updateBody.bind(this)}
+            />
+          </div>
+          <div className='bottom-panel'>
+            {this.state.editMode && (
+              <Button onClick={this.saveChanges.bind(this)}>Save</Button>
+            )}
+            {this.state.editMode && (
+              <Button onClick={this.removeChanges.bind(this)}>Cancel</Button>
+            )}
+            {!this.state.editMode && (
+              <Button onClick={this.setEditMode.bind(this, true)}>Edit</Button>
+            )}
+            <Button onClick={this.props.deleteItem}>Delete</Button>
+            {this.props.useComments &&
+              <Link to={`/category/${this.props.category}/${this.props.id}`} className='button small round comments-button'>
+                {`Comments (${this.props.commentCount})`}
+              </Link>
+            }
+          </div>
+        </fieldset>
+      </form>
     )
   }
 }
@@ -121,11 +115,44 @@ GenericBlogItem.propTypes = {
   updateItem: PropTypes.func.isRequired,
   upVoteItem: PropTypes.func.isRequired,
   downVoteItem: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+  useComments: PropTypes.bool,
   useTitle: PropTypes.bool
 }
 
-function RoundButton ({ onClick, children }) {
-  return <button className='button primary round outline' onClick={onClick}>{children}</button>
+class VoteScoreBadge extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      decreased: false,
+      increased: false
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const decreased = (nextProps.voteScore < this.props.voteScore)
+    const increased = (nextProps.voteScore > this.props.voteScore)
+    this.setState({ decreased, increased })
+  }
+
+  resetState () {
+    this.setState({
+      decreased: false,
+      increased: false
+    })
+  }
+
+  render () {
+    const className = 'label badge black vote-badge' + (
+      this.state.decreased ? ' quick-decrease' : this.state.increased ? ' quick-increase' : ''
+    )
+
+    return (
+      <span className={className} onAnimationEnd={this.resetState.bind(this)}>
+        {`${this.props.voteScore} votes`}
+      </span>
+    )
+  }
 }
 
 export default GenericBlogItem
